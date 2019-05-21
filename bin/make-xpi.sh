059bad3a43945d8effd8aa168efff3a16513fe27
Michael Tiernan <Michael.Tiernan@gMail.com>
2019-05-21 16:06:19 -0400

    First try of this.

--- bin/make-xpi.sh
+++ bin/make-xpi.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
-cd ../src
-zip -r ../dist/thunderbird_message_filter_import_export.xpi *
+
+set -u
+
+myRoot="${PWD}"
+
+if [ -d "${myRoot}/src" ] ; then
+	pushd "${myRoot}/src"
+elif [ -d "${myRoot}/../src" ] ; then
+	pushd "${myRoot}/../src" 
+else
+	echo "I'm lost."
+	exit 2
+fi
+
+if [ -z "$(which zip)" ] ; then
+	echo "We need the zip tool to do this."
+	exit 2
+fi
+
+. ../CurrVers
+
+((BRANCH+=1))
+
+{
+	echo "VERS=${VERS}"
+	echo "REL=${REL}"
+	echo "BRANCH=${BRANCH}"
+	echo "PATCH=${PATCH}"
+} > ../CurrVers
+
+echo "Updating version string."
+sed -e "s@<em:version>VERS</em:version>@<em:version>${VERS}.${REL}.${BRANCH}.${PATCH}</em:version>@" < ../install.rdf > install.rdf
+
+zip -rq ../dist/thunderbird_message_filter_import_export.xpi *
+
+/bin/ls -l ../dist/
